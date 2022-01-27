607c9294575fa581df5f0758e04b0e87555b0dec
Joe Shields <shields.john.joseph@gmail.com>
2022-01-27 16:23:34 -0600

    added button to connect to meta wallet

--- pages/index.js
+++ pages/index.js
@@ -3,7 +3,8 @@ import { useWeb3 } from '@3rdweb/hooks'
 
 
 export default function Home() {
+  const { address, connectWallet } = useWeb3()
   return (
-    <div>Heyo</div>
+    <div><button onClick={() => connectWallet('injected')}>Connect Wallet</button></div>
   )
 }
