3def019748d15a038cbf6054449588958acacdec
Joe Shields <shields.john.joseph@gmail.com>
2022-02-07 15:35:07 -0600

    imported main component to dashboard

--- pages/Dashboard.js
+++ pages/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '../components/Header';
 import styled from 'styled-components';
+import Main from '../components/Main';
 
 
 
@@ -9,6 +10,7 @@ const Dashboard = ({address}) => {
       <Wrapper>
           <MainContainer>
               <Header />
+              <Main />
           </MainContainer>
       </Wrapper>
   )
