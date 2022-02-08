50e3a8a1d296c5c9304020c33024670456dd1cf8
Joe Shields <shields.john.joseph@gmail.com>
2022-02-07 19:18:47 -0600

    tested coin table

--- static/coins.js
+++ static/coins.js
@@ -66,4 +66,14 @@ export const coins = [
     change: 200.24,
     allocation: 17.89,
   },
+  // {
+  //   name: 'WCD Token',
+  //   sign: 'WCD',
+  //   logo: ethLogo,
+  //   balanceUsd: 300,
+  //   balanceCoin: 6.35664236,
+  //   priceUsd: 9.22,
+  //   change: 25.24,
+  //   allocation: 17.89,
+  // },
 ]
