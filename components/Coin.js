ec79e4e032e249e68a7d1f506f0349b1fd30656c
Joe Shields <shields.john.joseph@gmail.com>
2022-02-07 18:54:25 -0600

    created coin icon and primary and secondary styled components

--- components/Coin.js
+++ components/Coin.js
@@ -23,3 +23,18 @@ const Wrapper = styled.div`
         padding: 1rem 2rem;
     }
 `
+const NameCol = styled.div`
+    display: flex;
+    align-items: center;
+`
+const CoinIcon = styled.div`
+    width: 1.8rem;
+    margin-right: 1rem;
+`
+const Primary = styled.div`
+    margin-bottom: 0.1rem;
+`
+const Secondary = styled.div`
+    color: #8a919e;
+    font-size: 0.8rem;
+`
