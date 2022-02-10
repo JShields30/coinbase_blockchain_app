ec0c3ca01c03a688ad41e2ed972b5e123255326e
Joe Shields <shields.john.joseph@gmail.com>
2022-02-10 00:26:06 -0600

    created balance chart file

---
+++ components/BalanceChart.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+import { Line } from 'react-chartjs-2';
+
+import React from 'react';
+
+const BalanceChart = () => {
+  return <div></div>;
+};
+
+export default BalanceChart;
