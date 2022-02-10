1de804fca16220ca76bd3f5c09b91f9921345522
Joe Shields <shields.john.joseph@gmail.com>
2022-02-10 00:22:12 -0600

    laid out styled components

--- components/Promos.js
+++ components/Promos.js
@@ -3,7 +3,21 @@ import styled from 'styled-components';
 
 const Promos = () => {
   return <Wrapper>
-      
+      <OfferCard>
+          <Title>Yield Earned</Title>
+          <Description>Earn up to 2.8% APY on your crypto</Description>
+          <Placeholder />
+          <Additional style={{ fontSize: '1.5rem' }}>
+              $0.000066 <span>2.84% APY</span>
+          </Additional>
+      </OfferCard>
+
+      <OfferCard>
+          <Title>Learn and Earn</Title>
+          <Description>Earn up to 2.84% APY on your crypto</Description>
+          <Placeholder />
+          <Additional style={{ color: '#3773f5' }}>Verify Identity</Additional>
+      </OfferCard>
   </Wrapper>
 };
 
