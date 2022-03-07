3533a5537100c9161e84fa48d24e8cc5c3f1daca
Joe Shields <shields.john.joseph@gmail.com>
2022-03-07 14:10:22 -0600

    created coin selector modal file to pull in list of available crypto to be sent

---
+++ components/modal/CoinSelector.js
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import styled from "styled-components"
+
+const CoinSelector = (
+    {
+        
+    }
+    ) => {
+  return (
+    <div>CoinSelector</div>
+  )
+}
+
+export default CoinSelector
