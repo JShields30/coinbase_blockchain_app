4c6c693cf60d9e66485f61be2b86c159d7df02fa
Joe Shields <shields.john.joseph@gmail.com>
2022-03-07 15:54:19 -0600

    created wrapper title and coin list styled components

--- components/modal/CoinSelector.js
+++ components/modal/CoinSelector.js
@@ -14,3 +14,16 @@ const CoinSelector = (
 }
 
 export default CoinSelector
+
+const Wrapper = styled.div``
+
+const Title = styled.div`
+    width: 100%;
+    text-align: center;
+    font-size: 1.5rem;
+    margin-bottom: 1rem;
+`
+const CoinList = styled.div`
+    display: flex;
+    flex-direction: column;
+`
