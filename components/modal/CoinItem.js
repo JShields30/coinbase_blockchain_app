1e53fd966e6dc8124574c8960b382a797afc8891
Joe Shields <shields.john.joseph@gmail.com>
2022-03-07 18:29:51 -0600

    created styled components main icon name details name balance and is selected for pay with portion of send modal

--- components/modal/CoinItem.js
+++ components/modal/CoinItem.js
@@ -11,8 +11,41 @@ const CoinItem = ({
     thirdWebTokens,
 }) => {
   return (
-    <div>{sanityTokens[0].name}</div>
+    <div>{sanityTokens[0].symbol}</div>
   )
 }
 
 export default CoinItem
+
+const Main = styled.div`
+    flex: 1;
+    display: flex;
+    align-items: center;
+`
+const Icon = styled.div`
+    margin-right: 1rem;
+    height: 1.8rem;
+    width: 1.8rem;
+    border-radius: 50%;
+    overflow: hidden;
+    display: grid;
+    place-items: center;
+
+    & > img {
+        height: 120%;
+        width: 120%;
+        object-fit: cover;
+    }
+`
+const NameDetails = styled.div``
+
+const Name = styled.div`
+    font-size: 1.1rem;
+    margin-bottom: 0.2rem;
+`
+const Balance = styled.div``
+
+const IsSelected = styled.div`
+    margin-left: 0.5rem;
+    color: #3773f5;
+`
