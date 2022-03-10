db46e3c8a48279dba5a8429f20737fae69195b7b
Joe Shields <shields.john.joseph@gmail.com>
2022-03-10 16:45:42 -0600

    imported usestate useeffect styled client bicopy facheck and image url builder

--- components/modal/Receive.js
+++ components/modal/Receive.js
@@ -1,4 +1,10 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import styled from 'styled-components'
+import imageUrlBuilder from '@sanity/image-url'
+import { client } from '../../lib/sanity'
+import { BiCopy } from 'react-icons/bi'
+import { FaCheck } from 'react-icons/fa'
+
 
 const Receive = () => {
   return (
